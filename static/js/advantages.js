5f75db598398f55893c7ade7777ef71496d9d647
Rivaric <helium@internet.ru>
2025-01-26 22:35:50 +0300

    feat: add animation show for advantages

---
+++ static/js/advantages.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const items = document.querySelectorAll('.advantages__item');
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.5 
+    });
+  
+    items.forEach(item => observer.observe(item));
+});
